e5df7c8bf19a09f2929887715cd496ce3c6f0519
nagacoder <nagacoder@outlook.com>
2023-11-01 23:32:12 +0700

    init db

---
+++ backend/database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Creating Super Admin User
+        $superAdmin = User::create([
+            'name' => 'Javed Ur Rehman', 
+            'email' => 'superadmin@superadmin.com',
+            'password' => Hash::make('password')
+        ]);
+        $superAdmin->assignRole('Super Admin');
+
+        // Creating Admin User
+        $admin = User::create([
+            'name' => 'Syed Ahsan Kamal', 
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password')
+        ]);
+        $admin->assignRole('Admin');
+    }
+}
